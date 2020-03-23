9e03ab7067661a5c57f10da91bf736016809fa22
theDreamerNYM <akhtaruzzamankhan016@gmail.com>
2020-03-23 18:38:21 +0600

    some modification done on nav bar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
@@ -7,57 +7,37 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className="navbar navbar-expand navbar-light bg-white py-2 fixed-top">
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-white">
-          <Link to="/">
-
+        <Link to="/">
           <img
-              src="https://res.cloudinary.com/dllb2cjw6/image/upload/v1584737489/logo2_ikwqnv.png"
-              alt=""
-              height="50px"
-            />
-          
-          </Link>
-           
-          
+            src="https://res.cloudinary.com/dllb2cjw6/image/upload/v1584737489/logo2_ikwqnv.png"
+            alt=""
+            height="50px"
+          />
+        </Link>
 
-          <div className="collapse navbar-collapse d-flex justify-content-end">
-
-
-            <Link to="/checkout">
-
-            <button className="btn btn-link text-secondary text-dark">
-              <FontAwesomeIcon icon={faCartPlus}/> 0
-            </button>
-            
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item active">
+            <Link to="/checkout" className="nav-link">
+              <FontAwesomeIcon className="cart-icon" icon={faCartPlus} /> 0
             </Link>
-            
+          </li>
 
-
-
+          <li className="nav-item">
             <Link to="/login" className="nav-link">
-
-            <button
-              type="button"
-              className="btn btn-link text-secondary text-dark loginButton"
-            >
               Login
-            </button>
-
             </Link>
+          </li>
 
+          <li className="nav-item">
             <Link to="/signup">
-            <button className="btn btn-danger rounded-pill">Sign up</button>
+              <button className="btn btn-danger rounded-pill">Sign up</button>
             </Link>
-            
-            
-          </div>
-        </nav>
+          </li>
+        </ul>
       </div>
-
-      
-    </div>
+    </nav>
   );
 };
 
