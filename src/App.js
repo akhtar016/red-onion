6e20b23a396ba03af9a520a20ed271357d11ac00
theDreamerNYM <akhtaruzzamankhan016@gmail.com>
2020-04-05 20:16:43 +0600

    payment method integrated

--- src/App.js
+++ src/App.js
@@ -11,46 +11,36 @@ import ParticularItemDetails from "./Components/ParticularItemDetails/Particular
 import TopBanner from "./Components/TopBanner/TopBanner";
 import { AuthContextProvider } from "./Components/Login/useAuth";
 import AddData from "./Components/AddData/AddData";
-
-
+import Footer from "./Components/Footer/Footer";
+import Checkout from "./Components/Checkout/Checkout";
 
 function App() {
-
   const [cart, setCart] = useState([]);
-  
 
   const cartHandler = (data) => {
-    console.log(data)
-    const addedAlready = cart.find(cart => cart.key === data.key);
-    const newCart = [...cart, data]
+    console.log(data);
+    const addedAlready = cart.find((cart) => cart.key === data.key);
+    const newCart = [...cart, data];
     setCart(newCart);
-    if(addedAlready){
-      const remainingCart = cart.filter(cart => cart.key !== data);
+    if (addedAlready) {
+      const remainingCart = cart.filter((cart) => cart.key !== data);
       setCart(remainingCart);
-    }else{
+    } else {
       const newCart = [...cart, data];
       setCart(newCart);
     }
-  }
-
-
-  // console.log(cart)
-  // console.log(cart.length);
-
-  
-
-  
+  };
 
   return (
     <div>
       <AuthContextProvider>
-      
         <Router>
           <Switch>
             <Route exact path="/">
               <Navbar cart={cart}></Navbar>
               <TopBanner></TopBanner>
-              <AllFoods  cart={cart}></AllFoods>
+              <AllFoods cart={cart}></AllFoods>
+              <Footer/>
             </Route>
 
             <Route path="/login">
@@ -61,9 +51,15 @@ function App() {
               <SignUp></SignUp>
             </Route>
 
+            <Route path="/checkout">
+              <Checkout></Checkout>
+            </Route>
+
             <Route path="/food/:key">
               <Navbar cart={cart}></Navbar>
-              <ParticularItemDetails cartHandler={cartHandler}></ParticularItemDetails>
+              <ParticularItemDetails
+                cartHandler={cartHandler}
+              ></ParticularItemDetails>
             </Route>
 
             <Route path="/addData">
@@ -75,7 +71,7 @@ function App() {
             </Route>
           </Switch>
         </Router>
-        </AuthContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Footer.css'
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark py-3">
+            <div className="container">
+                <div className="row footer-top py-5">
+                    <div className="col-md-6 mb-5">
+                        <img src="https://res.cloudinary.com/dllb2cjw6/image/upload/v1584915160/logo2_tmrf40.png" alt=""/>
+                    </div>
+                    <div className="col-md-3">
+                        <ul className="list-unstyled">
+                            <li><Link to="/">About Online Food</Link></li>
+                            <li><Link to="/">Read Our Blog</Link></li>
+                            <li><Link to="/">Sign up to deliver</Link></li>
+                            <li><Link to="/">Add your restaurant</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3">
+                        <ul className="list-unstyled">
+                            <li><Link to="/">Get Help</Link></li>
+                            <li><Link to="/">Read FAQ</Link></li>
+                            <li><Link to="/">View All Cities</Link></li>
+                            <li><Link to="/">Restaurants near me</Link></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="footer-bottom d-flex justify-content-between align-items-center">
+                    <small className="text-secondary">Copyright &copy;  2020 Red Onion Restaurant </small>
+                    <ul className="list-inline">
+                        <li className="list-inline-item ml-3"><a href="/">Privacy Policy</a></li>
+                        <li className="list-inline-item  ml-3"><a href="/">Terms of Use</a></li>
+                        <li className="list-inline-item  ml-3"><a href="/">Pricing</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
